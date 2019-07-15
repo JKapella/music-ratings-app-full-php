9d7d3c69919f33426f5736a26cd2d807edaa402f
Jkapella <jvkapella@gmail.com>
2019-07-15 18:15:44 +0100

    created index page, added psudeocode and HTML head

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+//check if logged in
+
+//if logged in - go to the main page
+
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Music Ratings App</title>
+    <meta name="description" content="Keep track of your favourite music">
+    <meta name="author" content="JKapella">
+
+</head>
+
+<body>
+
+</body>
+</html>
