342680af3d17e61a1ce17e2ae741ba1e30318c6e
Jkapella <jvkapella@gmail.com>
2019-07-15 20:17:47 +0100

    created DBConnection class

---
+++ Classes/DbConnection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MusicRatings;
+
+Class DbConnection 
+{
+    private $dbConnection;
+
+    public function __construct(PDO $dbConnection)
+    {
+        $this->dbConnection = $dbConnection;
+    }
+
+    public function getDbConnection() 
+    {
+        return $this->dbConnection;
+    }
+
+}
