7eee75736a266c1e077c27daefa14abb00c6bfc2
Jkapella <jvkapella@gmail.com>
2019-07-16 18:30:59 +0100

    added check credentials to Loginhandler

--- Classes/LoginHandler.php
+++ Classes/LoginHandler.php
@@ -13,6 +13,13 @@ Class LoginHandler
 
     public function checkCredentials(string $username, string $password) 
     {
-        
+        $success = false;
+        $query = $this->db->prepare("SELECT `password` FROM `users` WHERE `username` = :username;");
+        $query->bindParam(':username', $username);
+        if ($query->execute() === true) {
+            $storedPassword = $query->fetchAll();
+            $success = password_verify($password, $storedPassword);
+        }
+        return $success;
     }
 }
