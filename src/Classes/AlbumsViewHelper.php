e44c7245c95fcc768d902d4c0470e938f4add7c3
Jkapella <jvkapella@gmail.com>
2019-07-23 19:19:21 +0100

    Changed results div to table

--- src/Classes/AlbumsViewHelper.php
+++ src/Classes/AlbumsViewHelper.php
@@ -6,15 +6,29 @@ class AlbumsViewHelper
 {
     public static function printAllAlbums(array $albums) 
     {
-        $html = "";
+        $html = "<table>
+        <th>Artist</th>
+        <th>Release</th>
+        <th>Format</th>
+        <th>Genre</th>
+        <th>Other Genre</th>
+        <th>Year</th>
+        <th>Rating</th>
+        <th></th>";
         foreach ($albums as $album) {
             $properties = $album->getProperties();
-            $html .= "<div class='entry-container'>";
-            foreach($properties as $field => $value) {
-                $html .= "$field: $value,";
-            }    
-            $html .= "</div>";
+            $html .= "  <tr data-id='" . $properties['id'] . "'>
+            <td>" . $properties['artist'] . "</td>
+            <td>" . $properties['release'] . "</td>
+            <td>" . $properties['format'] . "</td>
+            <td>" . $properties['genre'] . "</td>
+            <td>" . $properties['other_genre'] . "</td>
+            <td>" . $properties['year'] . "</td>
+            <td>" . $properties['rating'] . "</td>
+            <td></td>   
+            </tr>";
         }
+        $html .= "</table>";
         return $html;
     }
 }
