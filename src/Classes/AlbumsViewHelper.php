8b283394248f9e19295af31046355822056fc494
Jkapella <jvkapella@gmail.com>
2019-07-23 19:26:55 +0100

    changed individual album back to divs

--- src/Classes/AlbumsViewHelper.php
+++ src/Classes/AlbumsViewHelper.php
@@ -6,29 +6,24 @@ class AlbumsViewHelper
 {
     public static function printAllAlbums(array $albums) 
     {
-        $html = "<table>
-        <th>Artist</th>
-        <th>Release</th>
-        <th>Format</th>
-        <th>Genre</th>
-        <th>Other Genre</th>
-        <th>Year</th>
-        <th>Rating</th>
-        <th></th>";
+        $html = "";
         foreach ($albums as $album) {
             $properties = $album->getProperties();
-            $html .= "  <tr data-id='" . $properties['id'] . "'>
-            <td>" . $properties['artist'] . "</td>
-            <td>" . $properties['release'] . "</td>
-            <td>" . $properties['format'] . "</td>
-            <td>" . $properties['genre'] . "</td>
+            $html .= "  <div class='entry-container' data-id='" . $properties['id'] . "'>
+            <h3>" . $properties['artist'] . " - 
+            <em>" . $properties['release'] . "</em>";
+            if ($properties['format'] != "") {
+                $html .= " (" . $properties['format'] . ")";
+            }
+            $html .= "</h3>";
+            
+            $html .= "<td>" . $properties['genre'] . "</td>
             <td>" . $properties['other_genre'] . "</td>
             <td>" . $properties['year'] . "</td>
             <td>" . $properties['rating'] . "</td>
             <td></td>   
-            </tr>";
+            </div>";
         }
-        $html .= "</table>";
         return $html;
     }
 }
