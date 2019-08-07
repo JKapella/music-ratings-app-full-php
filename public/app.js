8002ae80e737f21ebce533c5aaa7ce9055b2f285
Jkapella <jvkapella@gmail.com>
2019-08-07 18:30:41 +0100

    added event listener for new entry button

--- public/app.js
+++ public/app.js
@@ -4,16 +4,19 @@ function setUpButtonFunctionality() {
 
     let unlistenedButton = document.getElementById('unlistenedButton')
     unlistenedButton.addEventListener('click', switchToUnistenedEntries)
+
+    let newEntryButton = document.getElementById('newEntryButton')
+    newEntryButton.addEventListener('click', switchToNewEntry)
 }
 
 function switchToListenedEntries() {
     let entries = document.querySelectorAll('.entry-container')
     entries.forEach(element => {
-        element.classList.remove('visible', 'hidden');
+        element.classList.remove('visible', 'hidden')
         if (element.dataset.listened === 'yes') {
-            element.classList.add('visible');
+            element.classList.add('visible')
         } else {
-            element.classList.add('hidden');
+            element.classList.add('hidden')
         }
     })
 }
@@ -21,13 +24,21 @@ function switchToListenedEntries() {
 function switchToUnistenedEntries() {
     let entries = document.querySelectorAll('.entry-container')
     entries.forEach(element => {
-        element.classList.remove('visible', 'hidden');
+        element.classList.remove('visible', 'hidden')
         if (element.dataset.listened === 'no') {
-            element.classList.add('visible');
+            element.classList.add('visible')
         } else {
-            element.classList.add('hidden');
+            element.classList.add('hidden')
         }
     })
 }
 
-setUpButtonFunctionality();
+function switchToNewEntry() {
+    let entries = document.querySelectorAll('.entry-container')
+    entries.forEach(element => {
+        element.classList.remove('visible', 'hidden')
+        element.classList.add('hidden')
+    })
+}
+
+setUpButtonFunctionality()
