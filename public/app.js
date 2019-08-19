ff720fac636edf456d69df91d111021ebf1f05d1
Jkapella <jvkapella@gmail.com>
2019-08-19 18:47:05 +0100

    added psudocode and began coding validation in JS

--- public/app.js
+++ public/app.js
@@ -58,9 +58,35 @@ function submitNewEntry(e) {
     e.preventDefault()
     let formElements = document.querySelectorAll('.form-element')
 
-    //validation on elements!
-    //some sort of error messages!
-    //submit the thing!
+    let validatedEntries = validateNewEntry(formElements);
+
+    if (validatedEntries != '') {
+        //it's good!
+        //submit the entries!
+        //clear error messages
+        //confirm the entry has been submitted
+    } else {
+        //display the error message!
+    }
+}
+
+function validateNewEntry(formToValidate) {
+    
+    let formEmpty = true
+
+    formToValidate.forEach((formItem) => {
+
+        if (formItem.value != '') {
+            formEmpty = false
+        }
+    })
+
+    if (formEmpty) {
+        console.log('The Form Is Empty!')
+        //return error message - you need to put something in the form!
+    }
+
+    console.log(formEmpty)
 }
 
 setUpButtonFunctionality()
